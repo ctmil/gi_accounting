1d90542b2c5068a16c3cbc96c676a0a71eb7a48b
morrillo <gustavo.orrillo@moldeo.coop>
2017-01-12 15:35:21 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'GI Accounting',
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['base','account','branches'],
+    'data': [
+	#'account_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
