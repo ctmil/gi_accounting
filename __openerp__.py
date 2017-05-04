fdcd63cd657bf54092ed9e16fab6098f1414a3f8
morrillo <gustavo.orrillo@gmail.com>
2017-05-04 13:27:14 -0300

    Added hoy filter

--- __openerp__.py
+++ __openerp__.py
@@ -2,7 +2,7 @@
     'name': 'GI Accounting',
     'category': 'Sales',
     'version': '0.1',
-    'depends': ['base','account','branches','l10n_ar_invoice','pagos_tarjeta_credito'],
+    'depends': ['base','account','branches','l10n_ar_invoice','pagos_tarjeta_credito','l10n_ar_fpoc','account_voucher'],
     'data': [
 	'account_view.xml',
 	'account_report.xml',
